65576c63f4875bf6ec3416cebc3b50c346833403
MilanHaiman <mhaiman@stuy.edu>
2018-10-16 12:28:31 -0400

    (possibly broken) initial insert_ordered

--- musiclist.c
+++ musiclist.c
@@ -14,9 +14,7 @@ void print_list(struct song_node *first) {
 	printf("End List\n");
 }
 struct song_node * insert_front(struct song_node *first, char * n, char * a) {
-	struct song_node *ref = malloc(sizeof(struct song_node));
-	strncpy(ref->name,n,100);
-	strncpy(ref->artist,a,100);
+	struct song_node *ref = new_node(n,a);
 	ref->next = first;
 	return ref;
 }
@@ -36,8 +34,34 @@ int compare(struct song_node *a, struct song_node *b) {
 	}
 	return strcmp(a->artist,b->artist);
 }
-void insert_ordered(struct song_node *new) {
-	
+struct song_node * new_node(char * n, char * a) {
+	struct song_node *ref = malloc(sizeof(struct song_node));
+	strncpy(ref->name,n,100);
+	strncpy(ref->artist,a,100);
+	return ref;
+}
+struct song_node * insert_ordered(struct song_node *first, char * n, char * a) {
+	struct song_node *ref = new_node(n,a);
+	if (first == NULL) {
+		return ref;
+	}
+	if (compare(new_node,first) <= 0) {
+		ref->next = first;
+		return ref;
+	}
+	struct song_node *prev = first;
+	struct song_node *cur = prev->next;
+	while (cur != NULL) {
+		if (compare(new_node,cur) <= 0) {
+			ref->next = cur;
+			prev->next = ref;
+			return first;
+		}
+		prev = cur;
+		cur = cur->next;
+	}
+	prev->next = ref;
+	return first;
 }
 
 
--- musiclist.h
+++ musiclist.h
@@ -12,7 +12,8 @@ void print_list(struct song_node *first);
 struct song_node * insert_front(struct song_node *first, char * n, char * a);
 struct song_node * free_list(struct song_node *first);
 int compare(struct song_node *a, struct song_node *b);
-void insert_ordered(struct song_node *new);
+struct song_node * new_node(char * n, char * a);
+struct song_node * insert_ordered(struct song_node *first);
 
 
 

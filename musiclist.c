ad132c6df870a3f7628fd90c555bc86803ca756d
MilanHaiman <mhaiman@stuy.edu>
2018-10-19 00:02:10 -0400

    commented out

--- musiclist.c
+++ musiclist.c
@@ -47,7 +47,7 @@ int compare(struct song_node *a, struct song_node *b) {
 struct song_node * new_node(char * n, char * a) {
 	struct song_node *ref = malloc(sizeof(struct song_node));
 	strncpy(ref->name,n,100);
-	strncpy(ref->artist,a,100;
+	strncpy(ref->artist,a,100);
 	ref->next = NULL;
 	return ref;
 }

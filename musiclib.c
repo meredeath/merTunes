5578bd23736d3529997923b6d6ac162ccc50d4cb
MilanHaiman <mhaiman@stuy.edu>
2018-10-18 23:53:34 -0400

    commented out

--- musiclib.c
+++ musiclib.c
@@ -16,8 +16,7 @@ int get_index(char c) {
 
 void add_song(struct song_node *list[], char *n, char *a) {
 	int index = get_index(a[0]);
-	struct song_node *current = list[index];
-	list[index] = insert_ordered(current,n,a);
+	list[index] = insert_ordered(list[index],n,a);
 	
 }
 
